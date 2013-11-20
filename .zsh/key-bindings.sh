d3ca22c69b0c270dfee0ff3cffea300e065774ef
rejuvyesh <rejuvyesh@gmail.com>
2013-11-20 10:05:17 +0530

    end key works

--- .zsh/key-bindings.sh
+++ .zsh/key-bindings.sh
@@ -1,9 +1,13 @@
+bindkey -e
+
 # make search up and down work, so partially type and hit up/down to find relevant stuff
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
 
-# Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
-bindkey '^?' backward-delete-char
-bindkey "^[[3~" delete-char
-bindkey "^[3;5~" delete-char
-bindkey "\e[3~" delete-char
+
+bindkey '^[[3~' delete-char
+bindkey '[1~'  beginning-of-line
+bindkey '^[[4~' end-of-line
+bindkey '^[[5~' beginning-of-history
+bindkey '^[[6~' end-of-history
+bindkey '^[[33~' backward-kill-word
