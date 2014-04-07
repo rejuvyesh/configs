664335a86de2943115a4aedb4c45436677ab163e
rejuvyesh <mail@rejuvyesh.com>
2014-04-08 05:04:58 +0530

    scripts are already in path

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -268,9 +268,9 @@ keys' conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm, xK_f), spawn "downloadcovers.sh")
     -- screenshot
     -- screenshot screen
-    , ((modm .|. controlMask, xK_o), spawn "$HOME/dev/scripts/screenshot.sh scr")
+    , ((modm .|. controlMask, xK_o), spawn "screenshot.sh scr")
     -- screenshot window or area
-    , ((modm .|. shiftMask, xK_o), spawn "$HOME/dev/scripts/selection")
+    , ((modm .|. shiftMask, xK_o), spawn "selection")
 
     -- Brightness Control
     , ((0, xF86XK_MonBrightnessUp), spawn "light -aq 20")
